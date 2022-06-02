cbafb18e89c1a4925d6fbce646be073bfdc2e9a7
Maya Lorimer <maya.lorimer@yahoo.com>
2022-06-02 14:00:52 -0700

    Completed displayStorage function

--- assets/script2.js
+++ assets/script2.js
@@ -1,8 +1,26 @@
 var clearScoresBtn = document.querySelector(".clear-highscores");
+var highScoresList = document.querySelector(".highscoreslist")
+var highscoresArr = [];
 
 //clears the scores when clicked
 function clearScores() {
+    highScoresList.innerHTML = "";
     localStorage.removeItem('highscores');
 }
 
 clearScoresBtn.addEventListener("click", clearScores); 
+
+
+var highScoreDisplay = document.querySelector("highScores");
+displayStorage(); 
+
+// double check this
+function displayStorage(){
+    console.log("hello");
+    highscoresArr = JSON.parse(localStorage.getItem("highscores"));
+    console.log(highscoresArr); 
+    
+    for (var i = 0; i < highscoresArr.length; i++){
+        highScoresList.innerHTML += '<li>' + highscoresArr[i].initials + ": " + highscoresArr[i].score + '</li>';
+    }
+}
