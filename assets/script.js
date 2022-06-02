bc1998b2f1f1298c3589353f7ef537bfd15b84ec
Maya Lorimer <maya.lorimer@yahoo.com>
2022-06-01 19:41:36 -0700

    HTML completed

--- assets/script.js
+++ assets/script.js
@@ -3,41 +3,83 @@
 // list items of answers
 // end game div
 // timer (span)
+var timerElement = document.querySelector(".timer-count");
+var questions = document.querySelector(".question");
+var answers = document.querySelector(".answers");
 
 //general variables
 var score = 0; 
-var time;
+var time = 60;
 var timer;
+var questionIndex = 0; 
 //questions left (by rendering the indexes)
 var questionIndex = 0; 
 //feedback, correct/incorrect
 
-//event listener for start button
-//reference the button
-// add event listener and startGame()
+// variable to control the objects on the start screen
+var startScreen = document.querySelector(".start-screen");
 
-//startGame()
-// hide intro start screen
-//first question rendered to the screen
-    //call a function to renderQuestion
-//timer start and time display
+function startGame(){
+    // clears the homescreen from view
+    startScreen.setAttribute("style", "display: none");
+
+    // function for rendering the question to the screen
+    renderQuestion();
+
+    //starts the timer
+    startTimer();
+}
+
+//event listener for start button
+var startButton = document.querySelector(".start-button");
+startButton.addEventListener("click", startGame);
+
+
 // startTimer
-timer = setInterval(function, 1000);
+function startTimer(){
+    timer = setInterval(function(){
+        time--;
+        timerElement.textContent = time;
+
+        if (timerCount >= 0){
+            //fill out what happens if there is more time left
+            //check if the questions are done being answered
+        }
+
+        if (secondsLEft === 0){
+            clearInterval(timerInterval);
+            // lose function
+        }
+    })
+}
+
+//array that holds the questions
+var questionsArr = [
+    {
+        title: "String values must be enclosed in __ in javascript.",
+        answerChoices: ["quotes", "curly braces", 'parenthesis', 'backslashes'],
+        correctanswer: "quotes" //compare to the users choice 
+    }, 
+    {
+        title: "Common data types DO NOT include ___.",
+        answerChoices: ["boolean", "alerts", "string", "number"],
+        correctanswer: "alerts"
+    }
+]
 
 // renderQuestion()
 function renderQuestion(){
+    questions.textContent = questionsArr[questionIndex].title; 
+    for (var j = 0; j < 4; j++){
+        var li = document.createElement('li');
+        var text = document.createTextNode(questionsArr[questionIndex].answerChoices[j]);
+        li.appendChild(text);
+        answers.appendChild(li);
+    }
+
 // change hidden value of questions div
 // variable with array of objects to hold questions
-var questions = [
-    {
-        title: "JS question",
-        answerChoices: ["1", "2", '3', '4'],
-        correctanswer: "answer" //compare to the users choice 
-    }, 
-    {
-        title: 
-    }
-]
+
 }
 
 //reference the index of hte question to display on the screen, start at 0 and loop through
@@ -49,15 +91,22 @@ var questions = [
 
 //add event listeners to the answer choices or div that holds the answer choices using event.target and if matches the correct answer
 // listen for click, make event.target a variable, conditional to see if it is equal to the correct answer, provide feedback
+
+var correctFeedback = document.querySelector(".correct");
+var incorrectFeedback = document.querySelector(".incorrect");
+
 var selectedAns = event.target;
 
-if(selectedAns === question[0].correctAnswer){
+if(selectedAns === questionsArr[0].correctanswer){
     // provide correct feedback
+    correctFeedback.setAttribute("style", "display: default");
     // DOM add an element
 }
 else {
     // incorrect feedback comes up
+    incorrectFeedback.setAttribute("style", "display: default");
     // global time variable is substracted from
+    time =- 10; 
 }
 
 
